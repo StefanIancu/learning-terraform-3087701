2cec935ff9136707becba2bdd76f4c7be13beeb4
Stefan Iancu <124818078+StefanIancu@users.noreply.github.com>
2023-08-17 14:18:21 +0300

    vpc finally done

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "custom_vpc" {
+module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -45,7 +45,7 @@ resource "aws_security_group" "blog" {
   tags = {
     Terraform = "true"
   }
-  vpc_id = module.custom_vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
@@ -82,7 +82,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.vpc.public_subnets[0]
+  vpc_id  = module.vpc.vpc_id
   name    = "blog"
 
   ingress_rules = ["https-443-tcp","http-80-tcp"]
