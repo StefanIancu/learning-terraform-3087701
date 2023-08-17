fa1cfb9a35d1e77ca9951445a0943c3857b7eff9
Stefan Iancu <124818078+StefanIancu@users.noreply.github.com>
2023-08-17 14:34:15 +0300

    load balancer

--- main.tf
+++ main.tf
@@ -28,6 +28,49 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = data.aws_vpc.default.id
+  subnets            = ["subnet-abcde012", "subnet-bcde012a"]
+  security_groups    = module.blog_sg.security_group_id
+
+
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
