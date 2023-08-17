28a0ec399d01a7a074a3116f7488c62fbd81de86
Stefan Iancu <124818078+StefanIancu@users.noreply.github.com>
2023-08-17 14:45:07 +0300

    fixed autoscaling

--- main.tf
+++ main.tf
@@ -27,8 +27,8 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = data.aws_vpc.default.id
-  target_groups_arns  = module.blog_lb.target_groups_arns
-  security_group = [module.blog_sg.security_group_id]
+  target_group_arns  = module.blog_lb.target_groups_arns
+  security_groups = [module.blog_sg.security_group_id]
 
   image_id               = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
